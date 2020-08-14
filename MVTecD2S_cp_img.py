1d9670bf51f605dc337ef78292e4c8f203d8e803
edwardnguyen1705 <thanhng1985@gmail.com>
2020-08-14 22:16:56 +0700

    working on free server

--- MVTecD2S_cp_img.py
+++ MVTecD2S_cp_img.py
@@ -5,13 +5,14 @@ import os
 import shutil
 import json
 
-data_path = "/Users/thanhnguyen/Documents/Datasets/MVTec/D2S"
+data_path = "/home/thanh_nguyen/Datasets/MVTecD2S_v1"
+data_path_save = "/home/thanh_nguyen/Datasets/MVTecD2S_v2"
 
 # Specify image and annotation directories
-img_dir = os.path.join(data_path, 'images_org')
-ann_dir = os.path.join(data_path, 'annotations_org')
+img_dir = os.path.join(data_path, 'images')
+ann_dir = os.path.join(data_path_save, 'r_annotations_v2')
 
-splits = ['D2S_validation', 'D2S_test_info']
+splits = ['D2S_augmented', 'D2S_training_augmented', 'D2S_training', 'D2S_validation', 'D2S_test_info']
 
 for split in splits:
     annFile = os.path.join(ann_dir, f'{split}.json')
@@ -24,9 +25,9 @@ for split in splits:
     for img_dct in images:
         fname = img_dct['file_name'] 
 
-        src = os.path.join(data_path, 'images_org', fname)
+        src = os.path.join(data_path, 'images', fname)
 
-        output_path = os.path.join(data_path, 'images', split)
+        output_path = os.path.join(data_path_save, 'images', split)
 
         if not os.path.exists(output_path):
             os.makedirs(output_path)
